b6aeaa702f206d72f3d6808a8604c9a50eb1136f
sampsontan <sampsontan@gmail.com>
2020-06-22 21:39:14 +0800

    chk if there is autocomplete

--- server/forms.py
+++ server/forms.py
@@ -1,6 +1,7 @@
 from datetime import date
 
 from django import forms
+import autocomplete_light   #20200622
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 
