c9504a8809e652980d8c24dbb3c0e5ab0c869d3c
sampsontan <sampsontan@gmail.com>
2020-06-25 10:14:24 +0800

    misc

--- server/forms.py
+++ server/forms.py
@@ -1,5 +1,5 @@
 from datetime import date
-
+import autocomplete_light   #20200625
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
